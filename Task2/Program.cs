a7eb0cc3ec6b6e8cd87cae614ef3e9624071a1d2
Aysylu23 <aysbad25@gmail.com>
2023-10-28 15:31:38 +0500

    Add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+int tempNumber = number;
+int digitCount = 0;
+
+while(tempNumber > 0)
+{
+    digitCount++;
+    tempNumber /= 10;
+}
+
+Console.WriteLine($"{number} -> {digitCount}");
