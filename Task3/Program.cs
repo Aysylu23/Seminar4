e8ec5c8bcb685809c79e424c71fa442507e065b6
Aysylu23 <aysbad25@gmail.com>
2023-10-28 21:03:12 +0500

    Add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120*/
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+int result = 1;
+
+for(int i = 1; i <= number; i++)
+{
+    result *= i;
+}
+
+Console.WriteLine($"{number} -> {result}");
