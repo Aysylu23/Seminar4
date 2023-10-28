9eca3abaea4539653427fca6aafa43ff12b7e84a
Aysylu23 <aysbad25@gmail.com>
2023-10-28 15:29:42 +0500

    Add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int sum = 0;
+
+/*int i = 1;
+while(i <= number)
+{
+    sum += i;
+    i++;
+}*/
+for (int i = 1; i < number; i ++)
+{
+    sum +=1;
+}
+
+Console.WriteLine($"{number} -> {sum}");
