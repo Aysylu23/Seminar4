151c852d8182498d5a7ea0616ed737a8840cb6f3
Aysylu23 <aysbad25@gmail.com>
2023-10-28 15:28:28 +0500

    Add homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int sum = 0;
+int temp = number;
+
+
+while (number/10 > 0) 
+{
+    sum = number % 10 + sum;
+    number = number / 10;
+
+    if(number/10 == 0) sum = sum + (number % 10);
+}
+Console.WriteLine($"{temp} -> {sum}");
