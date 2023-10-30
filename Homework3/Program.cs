3d4aceccb9b52360216072edb68781db32412cfb
Aysylu23 <aysbad25@gmail.com>
2023-10-30 19:33:23 +0500

    Add homework3

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -2,18 +2,23 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]*/
 
-int Readnum(string text);
-Console.Write(text);
-int number = Int32.Parse(numberstr);
-return number;
 
-int num1 = Readnum("Введите число");
-int num2 = Readnum("Введите число");
-int num3 = Readnum("Введите число");
-int num4 = Readnum("Введите число");
-int num5 = Readnum("Введите число");
-int num6 = Readnum("Введите число");
-int num7 = Readnum("Введите число");
-int num8 = Readnum("Введите число");
 
-Console.WriteLine($"[{num1}, {num2}, {num3}, {num4}, {num5}, {num6}, {num7}, {num9}]");
+
+int array = 8;
+Random randomizer = new Random();
+int[] numbers = new int[array];
+int minNumber = 0;
+int maxNumber = 99;
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = randomizer.Next(minNumber, maxNumber +1); 
+  
+}
+Console.Write("[");
+for(int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]},");
+}
+Console.Write("]");
